89883634a631c6f29c75ebeb32a4bde69206dabc
Renée Cai <reneecai@rescomp-15-312599.stanford.edu>
2016-04-14 11:57:26 -0700

    Completed trig.py

--- trig.py
+++ trig.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python
+#to run this file, type in the command line: "python trig.py"
 
 #The following statements are used to import numpy and matplotlib.
 import numpy as np
 import matplotlib.pyplot as plt
 
+#x assigns an array from 0 to pi with a step size of 0.01
+x = np.arange(0,np.pi,0.01)
+#y is assigned the function sin(x)
+y = np.sin(x)
 
-# TODO fill in this function
+#This plots the graph of sin(x) from 0 to pi 
+plt.plot(x,y)
+
+
+#The following function gives an approximate integral value by summing up many small rectangles of height y and width dx
 def integrate(y, dx):
-    pass
+    np.sum(y*dx) 
 
-# TODO write code here to setup arrays x and y = sin(x) and then plot them.
-# After this is done implement your integrate function above integrate y
+#z is assigned the function cos(x)
+z = np.cos(x)
+#This plots the graph of cos(x) from 0 to ppi
+plt.plot(x,z)
+
+#Testing the integrate function with sine and cosine 
+integrate(y,0.01)
+integrate(z,0.01)
+
+#Using a numpy function, "trapz" (trapezoidal rule), to approximate the integral
+#numpy.trapz(function,stepsize) 
+np.trapz(y,x)
+np.trapz(z,x) 
+    
+
+    
+    
+    
